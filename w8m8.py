15026bc94ce69f45e24140e18c8ab00837213206
Ludvig Noring <ludno249@student.liu.se>
2017-02-27 23:32:57 +0100

    👌

--- w8m8.py
+++ w8m8.py
@@ -1,34 +1,65 @@
 import time
-def ticktock(*args):
-	t = int(time.time() * 10) % 12
-	uni = u'\\U0001f55' + hex(t)[2:]
-	emoji = bytes(uni, 'raw_unicode_escape').decode('raw_unicode_escape')
-	print(emoji, '', *args, end='\r')
 
-def crabby(*args, done=None):
-	t = abs(6-(int(time.time() * 10) % 12))
-	emoji = '🦀'
-	if done is not None:
-		n = int(done*20)
-		out = '📍' + n*' ' + emoji + ' '*(20-n) + '🏁'
+def progressbar(progress, *args, start='[', end=']', marker='', fill='█', bg=' ',
+	length=20, verbose=True):
+
+	left = int(length * progress)
+	right = length - left
+
+	out = start + fill*left + marker + bg*right + end
+
+	if verbose:
+		out += ' {:.2f}%'.format(progress*100)
+
+	print(out, *args, end='\r')
+
+
+def crabby(*args, **kwargs):
+	progressbar(*args, start='🐚', end='🏁 ', marker='🦀', fill=' ', **kwargs)
+
+
+def loader(*args, start='[', end=' ]', marker='🦀', bg=' ',
+	length=16, progress=None, bounce=True, verbose=True):
+	
+	if bounce:
+		i = abs(length - int(time.time() * 10) % (2*length))
 	else:
-		out = t*' ' + emoji + ' '*(6 - t)
-	print(out, '', *args, end='\r')
+		i = int(time.time() * 10) % length
+
+	out = start + i*bg + marker + (length-i)*bg + end
+
+	if verbose and progress is not None:
+		out += ' {:.2f}%'.format(progress*100)
+	
+	print(out, *args, end='\r')
+
+def clocky(*args, **kwargs):
+	i = int(time.time() * 10) % 12
+	emoji = bytes([240, 159, 149, 144 + i]).decode('utf-8')
+
+	loader(*args, start='', end=' ', marker=emoji, bg='', **kwargs)
 
 
 
 
 if __name__ == '__main__':
-	n = 40
-	for i in range(n):
-		ticktock(i+1,'/',n)
-		time.sleep(0.1)
+	n = 497
 
 	for i in range(n):
-		crabby(i+1,'/',n)
-		time.sleep(0.1)
+		clocky(progress=(i+1)/n)
+		time.sleep(0.005)
+	print()
 
 	for i in range(n):
-		crabby(i/n, done=i/n)
-		time.sleep(0.1)
-	print(' '*80, end='\r')
+		loader(progress=(i+1)/n)
+		time.sleep(0.005)
+	print()
+
+	for i in range(n):
+		progressbar((i+1)/n)
+		time.sleep(0.005)
+	print()
+	for i in range(n):
+		crabby((i+1)/n)
+		time.sleep(0.005)
+	print()
