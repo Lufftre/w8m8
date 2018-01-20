ae0af1d424a128fe705c665c21bd19e31527ddd5
Ludvig Noring <ludno249@student.liu.se>
2018-01-20 12:01:26 +0100

    PyPiPrep

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'w8m8',
+  packages = ['w8m8'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Your best mate waiting,
+  author = 'Ludvig Noring',
+  author_email = 'ludvig.noring@gmail.com',
+  url = 'https://github.com/Lufftre/w8m8', # use the URL to the github repo
+  download_url = 'https://github.com/Lufftre/w8m8/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['progress', 'bar', 'spinner'], # arbitrary keywords
+  classifiers = [],
+)
