f033f24b90a654bb9464408b0cada7baeebdd8b4
Ludvig Noring <ludno249@student.liu.se>
2018-01-20 13:23:10 +0100

    nuså

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'w8m8',
   packages = ['w8m8'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.20c',
   description = 'Your best mate waiting',
   author = 'Ludvig Noring',
   author_email = 'ludvig.noring@gmail.com',
   url = 'https://github.com/Lufftre/w8m8', # use the URL to the github repo
-  download_url = 'https://github.com/Lufftre/w8m8/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Lufftre/w8m8/archive/0.20c.tar.gz', # I'll explain this in a second
   keywords = ['progress', 'bar', 'spinner'], # arbitrary keywords
   classifiers = [],
 )
--- w8m8/__init__.py
+++
@@ -1,1 +0,0 @@
-from w8m8 import *
