8bd280100501b6aeeb2a00e00085ff1b79f4bb5d
Ludvig Noring <ludno249@student.liu.se>
2018-03-19 14:14:43 +0100

    eta

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'w8m8',
-  packages = ['w8m8'], # this must be the same as the name above
-  version = '0.20c',
+  # packages = ['w8m8'], # this must be the same as the name above
+  py_modules=['w8m8'],
+  version = '0.30a',
   description = 'Your best mate waiting',
   author = 'Ludvig Noring',
   author_email = 'ludvig.noring@gmail.com',
   url = 'https://github.com/Lufftre/w8m8', # use the URL to the github repo
-  download_url = 'https://github.com/Lufftre/w8m8/archive/0.20c.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Lufftre/w8m8/archive/0.30a.tar.gz', # I'll explain this in a second
   keywords = ['progress', 'bar', 'spinner'], # arbitrary keywords
   classifiers = [],
 )
--- w8m8.py
+++ w8m8.py
@@ -2,9 +2,17 @@ import time
 
 def iterate(iterable, *args, out='', **kwargs):
 	l = len(iterable)
+	time_start = time.time()
+	last_tid_kvar = 0
 	for i,x in enumerate(iterable):
-		progressbar(i/l, out.format(x, i=i), *args, **kwargs)
+		procent_per_sek = (i/l)/(time.time() - time_start)
+		tid_kvar = (1-(i/l))/procent_per_sek if procent_per_sek > 0 else 0
+		smooth_tid_kvar = 1+(tid_kvar + last_tid_kvar)
+		tid_text = time.strftime("%H:%M:%S", time.gmtime(smooth_tid_kvar))
+
+		progressbar(i/l, out.format(x, i=i), tid_text, *args, **kwargs)
 		yield x
+
 	progressbar(1, out.format(x), *args, **kwargs)
 	print()
 
@@ -58,24 +66,26 @@ def clocky(*args, **kwargs):
 
 
 if __name__ == '__main__':
+	import random
 	n = 497
+	for i in iterate(range(n)):
+		time.sleep(random.randint(30,40)/1000)
+	# for i in range(n):
+	# 	clocky(progress=(i+1)/n)
+	# 	time.sleep(0.005)
+	# print()
 
-	for i in range(n):
-		clocky(progress=(i+1)/n)
-		time.sleep(0.005)
-	print()
+	# for i in range(n):
+	# 	loader(progress=(i+1)/n)
+	# 	time.sleep(0.005)
+	# print()
 
-	for i in range(n):
-		loader(progress=(i+1)/n)
-		time.sleep(0.005)
-	print()
-
-	for i in range(n):
-		progressbar((i+1)/n)
-		time.sleep(0.005)
-	print()
-	for i in range(n):
-		crabby((i+1)/n)
-		time.sleep(0.005)
-	print()
+	# for i in range(n):
+	# 	progressbar((i+1)/n)
+	# 	time.sleep(0.005)
+	# print()
+	# for i in range(n):
+	# 	crabby((i+1)/n)
+	# 	time.sleep(0.005)
+	# print()
 
